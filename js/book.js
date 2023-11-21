4b3803ade6c77bc0e92481ba6d4621c9e89c0b88
gvsax3322 <cosbe3822@gmail.com>
2023-11-21 09:28:07 +0900

    docs : 작업중

--- js/book.js
+++ js/book.js
@@ -1,22 +1,75 @@
 window.addEventListener("load", function () {
   console.log("오늘의 도서");
   //외부 데이터를 불러온다.
+  const fileName = "book.json";
+  function add(name) {
+    return name.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
   const xhr = new XMLHttpRequest();
 
-  // 2. html 태그를 백틱을 이용해서 만든다.
-  const htmlBookTag = ``;
+  xhr.open("GET", fileName);
+
+  xhr.send();
+
+  xhr.onreadystatechange = function (event) {
+    if (event.target.readyState === XMLHttpRequest.DONE) {
+      const res = event.target.response;
+      const json = JSON.parse(res);
+      makeHtmlTag(json);
+    }
+  };
+
+  function makeHtmlTag(_res) {
+    // 2. html 태그를 백틱을 이용해서 만든다.
+    let htmlBookTag = ``;
+    //_res에 담겨진 객체에서 total 을 보관한다.
+
+    // 우리가 몇번 반복(total)해야 하는지 안다.
+    for (let i = 0; i < _res.total; i++) {
+      const index = i + 1;
+      const obj = _res["good_" + index];
+      const tempTag = `
+      <div class="swiper-slide">
+        <div class="book-slide-item">
+        <a href="${obj.url}">
+          <div class="book-slide-img">
+            <img src="${obj.image}" alt="${obj.desc}" />
+          </div>
+          <div class="book-slide-item-f">
+            <div class="book-slide-desc">${obj.desc}</div>
+            <div class="book-slide-price">
+            ${add(obj.price)}<span>원</span>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+    `;
+      htmlBookTag += tempTag;
+    }
+    showHtml(htmlBookTag);
+  }
+  function showHtml(_html) {
+    //  swiper 태그에 백틱을 배치한다.
+    const bookSlide = ".book-slide .swiper-wrapper";
+    const tag = document.querySelector(bookSlide);
+    tag.innerHTML = _html;
+    makeSwiper();
+  }
 
   // 3. swiper 태그에 백틱을 배치한다.
-  const bookSlide = ".book-slide .swiper-wrapper";
 
   // 4. swiper 작동시킨다.
-  const swiperbook = new Swiper(".book-slide", {
-    slidesPerView: 5,
-    spaceBetween: 28,
-    navigation: {
-      nextEl: ".book-slide-wrap .slide-next-bt",
-      prevEl: ".book-slide-wrap .slide-prev-bt",
-    },
-    slidesPerGroup: 5,
-  });
+  function makeSwiper() {
+    // 4. swiper 작동시킨다.
+    const swiperBook = new Swiper(".book-slide", {
+      slidesPerView: 5,
+      spaceBetween: 28,
+      navigation: {
+        nextEl: ".book-slide-wrap .slide-next-bt",
+        prevEl: ".book-slide-wrap .slide-prev-bt",
+      },
+      slidesPerGroup: 5,
+    });
+  }
 });
--- js/live.js
+++ js/live.js
@@ -1,22 +1,110 @@
 window.addEventListener("load", function () {
-  console.log("라이브");
-  //외부 데이터를 불러온다.
+  // 1.외부 데이터를 불러온다.
+  // : 외부 데이터 파일명.json
+  const fileName = "live.json";
+  function add(name) {
+    return name.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  // 외부 데이터 가져올때 작성법
   const xhr = new XMLHttpRequest();
+  // 외부의 파일을 열어라
+  //Get 방식으로  파일을 열어준다.
+  xhr.open("GET", fileName);
+  //실제로 실행하자.
+  xhr.send();
+  // 데이터의 전송 상태를 체크
+  xhr.onreadystatechange = function (event) {
+    if (event.target.readyState === XMLHttpRequest.DONE) {
+      // consol/e.log("자료 가져옴", event.target.response);
+      // 코드가 가독성이 떨어지므로 변수에 담는다.
+      // 규칙은 const 브타 직상히지.
+      // const가 문제가 된다면 let으로 변경한다.
+      const res = event.target.response;
+      // res를 전달해서 html 태그를 만든다.
+      // 데이터를 전달해서 정리해서 전달하는 것이 관례
+      // 전달받은 문자열을 js에서
+      // JSON 테이터를 해석(parse)하여
+      // 객채화{원시데이터 묶음 }한다.
+      const json = JSON.parse(res);
+      makeHtmlTag(json);
+    }
+  };
 
-  // 2. html 태그를 백틱을 이용해서 만든다.
-  const htmlLiveTag = ``;
+  //html 태그를 만드는 기능
+  function makeHtmlTag(_res) {
+    // 2. html 태그를 백틱을 이용해서 만든다.
+    let htmlLiveTag = ``;
+    //_res에 담겨진 객체에서 total 을 보관한다.
 
-  // 3. swiper 태그에 백틱을 배치한다.
-  const liveSlide = ".live-slide .swiper-wrapper";
+    // 우리가 몇번 반복(total)해야 하는지 안다.
+    for (let i = 0; i < _res.total; i++) {
+      const index = i + 1;
+      const obj = _res["good_" + index];
+      let discount = obj.discount === "" ? "" : obj.discount + "%";
+      let price =
+        obj.price === ""
+          ? ""
+          : obj.price + `<span class="live-good-info-price-span">원<span>`;
+      const tempTag = `
+      <div class="swiper-slide">
+        <div class="live-link">
+          <a href="${obj.url}" alt="${obj.desc}" class="live-link-a">
+          <img src="${obj.image}" alt="" class="live-link-img" />
+            ${
+              obj.date === ""
+                ? `<div class="live-link-info">`
+                : `<div class="live-link-info live-link-info-bg">`
+            }
+              <div class="live-link-info-1">
+                <i>${obj.title}</i><em>${obj.desc}</em>
+              </div>
+              <div class="live-link-info-2">
+                <div class="live-link-info-2-a">${obj.date}</div>
+                <div class="live-link-info-2-b">${obj.hour}</div>
+              </div>
+              <a href="${obj.url2}">
+                <div class="live-link-info-3">
+                ${
+                  obj.simage === "" ? `` : ` <img src="${obj.simage}" alt="" />`
+                }
+                  <div class="live-link-info-3-a">
+                    ${obj.detail}<span class="live-good-info-price">
+                      <b>${discount}</b>
+                      <em>${add(price)}</em>
+                    </span>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </a>
+        </div>
+      </div>              
+    `;
+      htmlLiveTag += tempTag;
+    }
+    showHtml(htmlLiveTag);
+  }
 
-  // 4. swiper 작동시킨다.
-  const swiperlive = new Swiper(".live-slide", {
-    slidesPerView: 4,
-    spaceBetween: 28,
-    navigation: {
-      nextEl: ".live-slide-wrap .slide-next-bt",
-      prevEl: ".live-slide-wrap .slide-prev-bt",
-    },
-    slidesPerGroup: 4,
-  });
+  // html 출력 전용 기능을 만들자.
+  function showHtml(_html) {
+    //  swiper 태그에 백틱을 배치한다.
+    const liveSlide = ".live-slide .swiper-wrapper";
+    const tag = document.querySelector(liveSlide);
+    tag.innerHTML = _html;
+    makeSwiper();
+  }
+  // swiper 만들고 실행하기
+  function makeSwiper() {
+    // 4. swiper 작동시킨다.
+    const swiperTour = new Swiper(".live-slide", {
+      slidesPerView: 4,
+      spaceBetween: 27,
+      navigation: {
+        nextEl: ".live-slide-wrap .slide-next-bt",
+        prevEl: ".live-slide-wrap .slide-prev-bt",
+      },
+      slidesPerGroup: 4,
+    });
+  }
 });
